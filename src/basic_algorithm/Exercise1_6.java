a709fb3507f6f99d91b22dbeb99eebb3a4c7ad8a
7coco <coco.greenery@gmail.com>
2019-08-13 08:39:50 +0900

    演習1-6

---
+++ src/basic_algorithm/Exercise1_6.java
@@ -0,0 +1,16 @@
+package basic_algorithm;
+
+public class Exercise1_6 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int i = 1;
+
+		while (i <= n) {
+			i++;
+		}
+
+		System.out.println(i);
+	}
+
+}
