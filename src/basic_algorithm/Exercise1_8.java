03fac699437dad2bd7781d9c6d940ad6ef45e37e
7coco <coco.greenery@gmail.com>
2019-08-14 13:14:37 +0900

    演習1-8

---
+++ src/basic_algorithm/Exercise1_8.java
@@ -0,0 +1,12 @@
+package basic_algorithm;
+
+public class Exercise1_8 {
+
+	public static void main(String[] args) {
+		System.out.println(sum(7));
+	}
+
+	private static int sum(int n) {
+		return (1 + n) * n / 2;
+	}
+}
