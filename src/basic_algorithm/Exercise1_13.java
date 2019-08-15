dfd6d009e59891b92c829d32ff81e237648691ef
7coco <coco.greenery@gmail.com>
2019-08-15 16:29:54 +0900

    演習1-13

---
+++ src/basic_algorithm/Exercise1_13.java
@@ -0,0 +1,27 @@
+package basic_algorithm;
+
+public class Exercise1_13 {
+
+	public static void main(String[] args) {
+		System.out.print("   |");
+		for (int j = 1; j <= 9; j++) {
+			System.out.printf("%3d", j);
+		}
+		System.out.println();
+
+		System.out.print("---+");
+		for (int j = 0; j <= 9; j++) {
+			System.out.print("---");
+		}
+		System.out.println();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(" " + i + " |");
+			for (int j = 0; j <= 9; j++) {
+				System.out.printf("%3d", i + j);
+			}
+			System.out.println();
+		}
+	}
+
+}
