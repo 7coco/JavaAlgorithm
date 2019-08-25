bae6431f859fc13196712f986c1abaa231f20722
7coco <coco.greenery@gmail.com>
2019-08-26 02:37:08 +0900

    演習3-4のためのメソッドを SearchStringUtil に追加

--- src/utils/SearchStringUtil.java
+++ src/utils/SearchStringUtil.java
@@ -33,6 +33,34 @@ public class SearchStringUtil {
 
 	/**
 	 * 指定された配列の要素を先頭から順にスペースを三つ挟みながら並べた文字列を返します。
+	 * 先頭、中央、最後尾にそれぞれ指定された記号を表示します。
+	 * @param left 先頭のインデックス
+	 * @param center 中央のインデックス
+	 * @param right 最後尾のインデックス
+	 * @param leftMark 先頭に表示する記号
+	 * @param centerMark 中央に表示する記号
+	 * @param rightMark 末尾に表示する記号
+	 */
+	public static void printPointers(int left, int center, int right, String leftMark, String centerMark, String rightMark) {
+		System.out.print("   |");
+		for (int i = 0; i < left - 1; i++) {
+			System.out.print("    ");
+		}
+		System.out.print("  " + leftMark);
+
+		for (int i = left; i < center - 1; i++) {
+			System.out.print("    ");
+		}
+		System.out.print("   " + centerMark);
+
+		for (int i = center; i < right - 1; i++) {
+			System.out.print("    ");
+		}
+		System.out.println("  " + rightMark);
+	}
+
+	/**
+	 * 指定された配列の要素を先頭から順にスペースを三つ挟みながら並べた文字列を返します。
 	 * 複数回使用する場合は、複数回呼び出すのではなく、戻り値を変数に格納することを想定しています。
 	 */
 	public static String getElementsString(int[] array) {
