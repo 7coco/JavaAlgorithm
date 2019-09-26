fdcc3fb14f55e35aaee19a2e9b3ab2113f250fe3
7coco <coco.greenery@gmail.com>
2019-09-26 12:51:11 +0900

    演習5-9

--- src/recurrence/Exercise5_8.java
+++ src/recurrence/Exercise5_8.java
@@ -1,5 +1,7 @@
 package recurrence;
 
+import stack_and_queue.IntStack;
+
 public class Exercise5_8 {
 
 	private static boolean[] flag_a = new boolean[8];
@@ -12,18 +14,33 @@ public class Exercise5_8 {
 	}
 
 	private static void set(int i) {
-		for (int j = 0; j < 8; j++) {
-			if (flag_a[j] == false &&
-					flag_b[i + j] == false &&
-					flag_c[i - j + 7] == false) {
-				pos[i] = j;
-				if (i == 7) {
-					print();
-				} else {
-					flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = true;
-					set(i + 1);
-					flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = false;
+		int j;
+		IntStack stack = new IntStack(8);
+		outer:
+		while (true) {
+			j = 0;
+			while (true) {
+				while (j < 8) {
+					if (flag_a[j] == false &&
+							flag_b[i + j] == false &&
+							flag_c[i - j + 7] == false) {
+						pos[i] = j;
+						if (i == 7) {
+							print();
+						} else {
+							flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = true;
+							stack.push(i++);
+							continue outer;
+						}
+					}
+					j++;
 				}
+				if (--i == -1) {
+					return;
+				}
+				j = stack.pop();
+				flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = false;
+				j++;
 			}
 		}
 	}
